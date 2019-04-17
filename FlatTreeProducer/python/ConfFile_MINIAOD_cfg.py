3e155eb88da39514ee0b480c3bc89a8e09623b43
Kirill Skovpen <Kirill.Skovpen@cern.ch>
2019-04-17 11:17:54 +0200

    fix a typo

--- FlatTreeProducer/python/ConfFile_MINIAOD_cfg.py
+++ FlatTreeProducer/python/ConfFile_MINIAOD_cfg.py
@@ -191,14 +191,14 @@ na.runTauID()
 from PhysicsTools.PatUtils.tools.runMETCorrectionsAndUncertainties import runMetCorAndUncFromMiniAOD
 if is2017:
     runMetCorAndUncFromMiniAOD(process,
-                               isData = isData,
+                               isData = options.isData,
                                fixEE2017 = True,
                                fixEE2017Params = {'userawPt': True, 'ptThreshold':50.0, 'minEtaThreshold':2.65, 'maxEtaThreshold':3.139},
                                postfix = "ModifiedMET"
     )
 else:
     runMetCorAndUncFromMiniAOD(process,
-                               isData = isData,
+                               isData = options.isData,
                                postfix = "ModifiedMET"
     )
 
