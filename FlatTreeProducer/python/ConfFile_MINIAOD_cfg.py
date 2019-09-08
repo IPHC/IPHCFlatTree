4df3395969c1d731b5c73c99e1f7062f87203896
Kirill Skovpen <Kirill.Skovpen@cern.ch>
2019-09-08 22:55:02 +0200

    update config

--- FlatTreeProducer/python/ConfFile_MINIAOD_cfg.py
+++ FlatTreeProducer/python/ConfFile_MINIAOD_cfg.py
@@ -325,46 +325,16 @@ if is2018:
 #  Input  #
 ###########
 
+fileName = 'file:F24F2D5E-DDEC-E811-AF50-90B11C08AD7D.root'
+if is2017:
+    fileName = 'file:7C60AC2B-E76F-E811-9D60-0025905B860C.root'
+if is2018:
+    fileName = 'file:A912BBFA-D1A1-8544-A430-8C98C0767737.root'
+
 process.source = cms.Source("PoolSource",
     duplicateCheckMode = cms.untracked.string("noDuplicateCheck"), # WARNING -- for test only !
-    fileNames = cms.untracked.vstring(
-
-	 #'/store/mc/RunIIFall17MiniAODv2/THQ_ctcvcp_4f_Hincl_13TeV_madgraph_pythia8/MINIAODSIM/PU2017_12Apr2018_94X_mc2017_realistic_v14-v1/00000/D0DD519F-5903-E911-AF2E-24BE05CE2D41.root'
-	 #'/store/mc/RunIIFall17MiniAODv2/THW_ctcvcp_5f_Hincl_13TeV_madgraph_pythia8/MINIAODSIM/PU2017_12Apr2018_94X_mc2017_realistic_v14-v1/80000/A63966E5-9503-E911-A265-0025905C96A6.root'
-	 #'/store/data/Run2017B/SingleElectron/MINIAOD/17Nov2017-v1/70000/FEA5055B-5BDE-E711-AE69-FA163EA2F9E4.root' #SingleElectron2017B
-	 #'/store/mc/RunIIFall17MiniAODv2/TT_FCNC-TtoHJ_aTleptonic_HToWWZZtautau_eta_hct-MadGraph5-pythia8/MINIAODSIM/PU2017_12Apr2018_94X_mc2017_realistic_v14-v3/90000/F857B893-B9B0-E811-8CB0-002590760A10.root'
-         #'/store/mc/RunIIFall17MiniAOD/TTZToLLNuNu_M-10_TuneCP5_13TeV-amcatnlo-pythia8/MINIAODSIM/94X_mc2017_realistic_v10-v1/70000/DCAA6B21-72F4-E711-9249-0019B9CADC3B.root'
-	 #'/store/mc/RunIIFall17MiniAOD/WW_DoubleScattering_13TeV-pythia8_TuneCP5/MINIAODSIM/94X_mc2017_realistic_v10-v1/510000/F65EA021-0BE5-E711-B234-0025904C7F5E.root'
-	 #'/store/mc/RunIIFall17MiniAODv2/TTWJetsToLNu_TuneCP5_13TeV-amcatnloFXFX-madspin-pythia8/MINIAODSIM/PU2017_12Apr2018_94X_mc2017_realistic_v14-v1/40000/F69A63D0-5942-E811-A63D-0025905C2C86.root'
-	 #'/store/mc/RunIIFall17MiniAOD/TTWJetsToLNu_TuneCP5_PSweights_13TeV-amcatnloFXFX-madspin-pythia8/MINIAODSIM/94X_mc2017_realistic_v10-v1/40000/FC073098-B80E-E811-8092-02163E01A140.root'
-	 #'/store/mc/RunIIFall17MiniAODv2/ttHJetToNonbb_M125_TuneCP5_13TeV_amcatnloFXFX_madspin_pythia8/MINIAODSIM/PU2017_12Apr2018_94X_mc2017_realistic_v14-v1/70000/F8E7EC91-9742-E811-93FA-001E67792566.root'
-	 #'/store/mc/RunIIFall17MiniAODv2/WZTo3LNu_TuneCP5_13TeV-amcatnloFXFX-pythia8/MINIAODSIM/PU2017_12Apr2018_94X_mc2017_realistic_v14-v1/60000/FED343B0-D155-E811-82B2-001A649D4791.root'
-	 #'/store/mc/RunIIFall17MiniAODv2/ZZTo4L_13TeV_powheg_pythia8/MINIAODSIM/PU2017_12Apr2018_94X_mc2017_realistic_v14-v2/70000/FC88B59B-74BB-E811-AD46-44A842BE76F1.root'
-	 #'/store/mc/RunIIFall17MiniAOD/tZq_ll_4f_ckm_NLO_TuneCP5_PSweights_13TeV-amcatnlo-pythia8/MINIAODSIM/94X_mc2017_realistic_v10-v1/80000/FA92DEEA-460F-E811-A8B8-0CC47A4D7618.root'
-	 #'/store/mc/RunIIFall17MiniAODv2/tZq_ll_4f_ckm_NLO_TuneCP5_PSweights_13TeV-amcatnlo-pythia8/MINIAODSIM/PU2017_12Apr2018_94X_mc2017_realistic_v14-v1/20000/78B67B34-B941-E811-B2AE-0025905A6090.root'
-	 #'/store/mc/RunIIFall17MiniAOD/TTZToLL_M-1to10_TuneCP5_13TeV-amcatnlo-pythia8/MINIAODSIM/94X_mc2017_realistic_v10-v1/70000/E26F591A-40D9-E711-AC54-FA163E528917.root'
-	 #'/store/mc/RunIIFall17MiniAOD/TTWW_TuneCP5_13TeV-madgraph-pythia8/MINIAODSIM/PU2017_94X_mc2017_realistic_v11_ext1-v1/10000/E0DEF705-E343-E811-ACBD-001D09FDD91E.root'
-	 #'/store/mc/RunIIFall17MiniAODv2/ST_tWll_5f_LO_TuneCP5_PSweights_13TeV-madgraph-pythia8/MINIAODSIM/PU2017_12Apr2018_94X_mc2017_realistic_v14_ext1-v1/80000/F62C62D9-0E0C-E911-B1FE-00000919FE80.root'
-	 #'/store/mc/RunIIFall17MiniAOD/TTWH_TuneCP5_13TeV-madgraph-pythia8/MINIAODSIM/94X_mc2017_realistic_v10-v1/80000/CC80714B-6402-E811-BCCE-003048CB8584.root'
-	 #'/store/mc/RunIIFall17MiniAOD/TTGJets_TuneCP5_13TeV-amcatnloFXFX-madspin-pythia8/MINIAODSIM/94X_mc2017_realistic_v10-v1/60000/349D565A-ECED-E711-B9C3-20CF3027A582.root'
-	 #'/store/mc/RunIIFall17MiniAOD/TGJets_leptonDecays_TuneCP5_PSweights_13TeV-amcatnlo-pythia8/MINIAODSIM/PU2017_94X_mc2017_realistic_v11-v1/70000/0698E1CE-EA36-E811-AA8F-0CC47A4DECF6.root'
-	 #'/store/mc/RunIIFall17MiniAODv2/WGToLNuG_TuneCP5_13TeV-madgraphMLM-pythia8/MINIAODSIM/PU2017_12Apr2018_94X_mc2017_realistic_v14-v1/00000/A0850A0B-18D1-E811-AEC0-008CFAF293BC.root'
-	 #'/store/mc/RunIIFall17MiniAOD/WWW_4F_TuneCP5_13TeV-amcatnlo-pythia8/MINIAODSIM/94X_mc2017_realistic_v11-v1/80000/EE8E5B81-F91C-E811-B3D7-3417EBE52612.root'
-	 #'/store/mc/RunIIFall17MiniAOD/WWZ_4F_TuneCP5_13TeV-amcatnlo-pythia8/MINIAODSIM/94X_mc2017_realistic_v11-v1/30000/D6C269B5-B327-E811-A095-FA163EBF244F.root'
-	 #'/store/mc/RunIIFall17MiniAOD/WZZ_TuneCP5_13TeV-amcatnlo-pythia8/MINIAODSIM/94X_mc2017_realistic_v11-v1/60000/8C4051B9-881F-E811-B606-0CC47A4D76C0.root'
-	 #'/store/mc/RunIIFall17MiniAOD/ZZZ_TuneCP5_13TeV-amcatnlo-pythia8/MINIAODSIM/94X_mc2017_realistic_v11-v1/90000/7A2D14DC-0127-E811-921C-008CFAC93CC4.root'
-	 #'/store/mc/RunIIFall17MiniAOD/TTTT_TuneCP5_13TeV-amcatnlo-pythia8/MINIAODSIM/94X_mc2017_realistic_v10-v2/20000/02C3910E-F10A-E811-8C52-A4BF0112BDFC.root'
-	 #'/store/mc/RunIIFall17MiniAODv2/WpWpJJ_EWK-QCD_TuneCP5_13TeV-madgraph-pythia8/MINIAODSIM/PU2017_12Apr2018_94X_mc2017_realistic_v14-v1/100000/FADFCC52-2772-E811-A925-562F11ECDC1A.root'
-	 #'/store/mc/RunIIFall17MiniAOD/WW_DoubleScattering_13TeV-pythia8_TuneCP5/MINIAODSIM/94X_mc2017_realistic_v10-v1/510000/F65EA021-0BE5-E711-B234-0025904C7F5E.root'
-	 #'/store/mc/RunIIFall17MiniAOD/WZG_TuneCP5_13TeV-amcatnlo-pythia8/MINIAODSIM/94X_mc2017_realistic_v10-v1/40000/EA167C38-A213-E811-BB51-FA163E7ED29E.root'
-	 #'/store/mc/RunIIFall17MiniAOD/TTWH_TuneCP5_13TeV-madgraph-pythia8/MINIAODSIM/94X_mc2017_realistic_v10-v1/80000/CC80714B-6402-E811-BCCE-003048CB8584.root'
-	 #'/store/mc/RunIIFall17MiniAOD/TTTW_TuneCP5_13TeV-madgraph-pythia8/MINIAODSIM/94X_mc2017_realistic_v10-v3/40000/F826CD28-2E0A-E811-B6AF-1CB72C1B6568.root'
-	 #'/store/mc/RunIIFall17MiniAOD/GluGluHToZZTo4L_M125_13TeV_powheg2_JHUGenV7011_pythia8/MINIAODSIM/94X_mc2017_realistic_v10-v1/70000/E6C44E02-F7FA-E711-9134-24BE05CEED81.root'
-	 #'/store/mc/RunIIFall17MiniAOD/TTToSemiLeptonic_TuneCP5_PSweights_13TeV-powheg-pythia8/MINIAODSIM/94X_mc2017_realistic_v10-v1/60000/4EB10D50-DBE7-E711-BBDD-90B11C27E5BE.root'
-	 #'/store/mc/RunIISummer16MiniAODv3/ttHToNonbb_M125_TuneCUETP8M2_ttHtranche3_13TeV-powheg-pythia8/MINIAODSIM/PUMoriond17_94X_mcRun2_asymptotic_v3-v2/120000/F24F2D5E-DDEC-E811-AF50-90B11C08AD7D.root'
-         'file:F24F2D5E-DDEC-E811-AF50-90B11C08AD7D.root'
-	 )
-#         ,skipEvents=cms.untracked.uint32(59400)
+    fileNames = cms.untracked.vstring( fileName )
+#    ,skipEvents=cms.untracked.uint32(14000)
 )
 
 ############
