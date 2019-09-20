0974a47c2ac5f2f9d33a136d9cfefd06da7a44bf
Kirill Skovpen <Kirill.Skovpen@cern.ch>
2019-09-20 08:18:50 +0200

    update

--- FlatTreeProducer/test/PROD/submit.zsh
+++ FlatTreeProducer/test/PROD/submit.zsh
@@ -1,6 +1,6 @@
 #!/bin/env zsh
 
-ver="Salmon1"
+ver="Salmon2"
 
 slist="list.txt"
 pset="crabConfigTemplate.py"
@@ -22,7 +22,9 @@ do
   pubdn=$(echo "${spl[2]}_${spl[3]}" | sed 's%-%_%g')
   nam=$(echo "${spl[1]}" | sed 's%-%_%g')
   reqn=$(echo "${nam}_${pubdn}" | sed 's%RunIISummer16MiniAODv3-PUMoriond17_94X_mcRun2_asymptotic_v3.*%MC2016%g' |\
+  sed 's%RunIISummer16MiniAODv3_PUMoriond17_94X_mcRun2_asymptotic_v3.*%MC2016%g' |\
   sed 's%RunIIFall17MiniAODv2-PU2017_12Apr2018_94X_mc2017_realistic_v14.*%MC2017%g' |\
+  sed 's%RunIIFall17MiniAODv2_PU2017_12Apr2018_94X_mc2017_realistic_v14.*%MC2017%g' |\
   sed 's%RunIIAutumn18MiniAOD-102X_upgrade2018_realistic_v15.*%MC2018%g')
   
   cat ${pset} | sed "s%INPUTDATASET%${i}%g" \
